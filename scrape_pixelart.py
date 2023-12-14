e64290278f07c3e694f48e4daf47a8f9faaaddef
parsakhk <parsakhk88@gmail.com>
2023-12-14 15:11:09 +0330

    Gitignore added + Software will create results folder.

--- scrape_pixelart.py
+++ scrape_pixelart.py
@@ -3,7 +3,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import time
 import requests
-
+import os
 
 name_of_ball = str(input('What ball you want to get pixel art from?: '))
 compacted_with_url = name_of_ball.replace(' ', '_')
@@ -32,6 +32,9 @@ print('getting the links.')
 image_tags = wd.find_elements(By.XPATH, f"//h2[@class='pi-item pi-item-spacing pi-title']//span[@style='font-size:16px; line-height:120% !important; text-align: center; display: block; padding-top:2px']//span[@class='mw-default-size']//a[@href='/wiki/File:{compacted_with_url}-icon.png']//img")
 image_urls = [img.get_attribute('src') for img in image_tags]
 
+time.sleep(3)
+### generate about me and creating results folder.
+os.mkdir('results/')
 ### download images.
 print('Downloading...')
 time.sleep(2)
